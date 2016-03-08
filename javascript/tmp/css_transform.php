78eb98fce91bf07ee11f454be70b289adcdd6be7
Jun Hsieh <jun.github@ijun.org>
2016-03-08 10:04:52 -0800

    up

--- javascript/tmp/css_transform.php
+++ javascript/tmp/css_transform.php
@@ -1,46 +1,48 @@
-<style>
-.gan {
-  background: #000;
-  color: #fff;
-  width: 100px;
-  height: 100px;
-  position: absolute;
-}
+<html>
+  <head>
+    <style>
+    .sourceDiv {
+      background: #000;
+      color: #fff;
+      width: 100px;
+      height: 100px;
+      position: absolute;
+    }
 
-.gan2 {
-  top: 300px;
-  left: 700px;
-  background: #c2c3c4;
-  color: #fff;
-  width: 100px;
-  height: 100px;
-  position: absolute;
-}
+    .destinationDiv {
+      top: 300px;
+      left: 700px;
+      background: #c2c3c4;
+      color: #fff;
+      width: 100px;
+      height: 100px;
+      position: absolute;
+    }
 
-.movela {
-  transform: translate(600px, 400px);
-  transition-duration: 5s;
-}
-</style>
+    .moveTo600x400 {
+      transform: translate(600px, 400px);
+      transition-duration: 5s;
+    }
+    </style>
+    <script src="/javascript/jquery/jquery-1.10.2.min.js"></script>
+    <script>
+      $(document).ready(function() {
+        $('.sourceDiv').on('click', function(e){
+          //$(this).addClass('moveTo600x400');
 
-<div class="gan">test</div>
-<div class="gan2">test</div>
+          var pos = $('.destinationDiv').offset();
+          console.log(pos);
 
-<script src="/javascript/jquery/jquery-1.10.2.min.js"></script>
-
-<script>
-$('.gan').on('click', function(e){
-  //$(this).addClass('movela');
-
-  var pos = $('.gan2').offset();
-  console.log(pos);
-
-  $(this).css({
-    transform: 'translate(' + pos.left + 'px, ' + pos.top + 'px)',
-    'transition-duration': '3s',
-  });
-});
-
-</script>
-
-
+          $(this).css({
+            transform: 'translate(' + pos.left + 'px, ' + pos.top + 'px)',
+            'transition-duration': '3s',
+          });
+        });
+      });
+    </script>
+  </head>
+  <body>
+    <div class="sourceDiv">test</div>
+    <div class="destinationDiv">test</div>
+  </body>
+</html>
