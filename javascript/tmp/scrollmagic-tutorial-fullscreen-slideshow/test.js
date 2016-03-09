70e9c4075d422906a11e7449d6038ce280b00d53
Jun Hsieh <jun.github@ijun.org>
2016-03-08 16:53:44 -0800

    up

--- javascript/tmp/scrollmagic-tutorial-fullscreen-slideshow/test.js
+++ javascript/tmp/scrollmagic-tutorial-fullscreen-slideshow/test.js
@@ -9,18 +9,32 @@
 
   // Create scene
   $("section").each(function() {
-      new ScrollMagic.Scene({
-          triggerElement: this
-      })
-      .setPin(this)
-      .addIndicators({
-        colorStart: "rgba(0,0,0, 1)",
-        colorEnd: "rgba(0,0,0, 1)", 
-        colorTrigger : "rgba(0,0,0, 1)",
-        name: $(this).attr('id'),
-      })
-      .addTo(ctrl);
-   
+    if ($(this).attr('id') == "four") {
+      return true;
+    }
+
+    new ScrollMagic.Scene({
+      triggerElement: this,
+      duration: 0,
+    })
+    .setPin(this)
+    .addIndicators({
+      colorStart: "rgba(0,0,0, 1)",
+      colorEnd: "rgba(0,0,0, 1)", 
+      colorTrigger : "rgba(0,0,0, 1)",
+      name: $(this).attr('id'),
+    })
+    //.loglevel(3)
+    .addTo(ctrl);
   });
+
+  // This means that section#four gets a class .is-active when the body is 300% or 3x window heights out of the viewport.
+  // get window height
+  var wh = window.innerHeight;
+  new ScrollMagic.Scene({
+    offset: wh*3,
+  })
+  .setClassToggle("section#four", "is-active add-another-class")
+  .addTo(ctrl);
  
 })(jQuery);
